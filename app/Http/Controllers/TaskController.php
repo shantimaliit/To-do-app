7c987b459ae3a8fc367fd9fa68e9e0671a3499f5
Shantal Veness Daluzon <152834008+shantimaliit@users.noreply.github.com>
2025-09-03 13:31:51 +0800

    Add files via upload

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::orderBy('is_completed', 'asc') // uncompleted muna, tapos completed
+                    ->orderBy('created_at', 'desc')  // optional: bagong gawa sa taas
+                    ->get();
+
+        return view('tasks.index', compact('tasks'));
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        Task::create([
+            'title' => $request->title,
+        ]);  
+        return redirect('/');
+    }
+
+    public function update(Task $task)
+    {
+        $task->update([
+            'is_completed' => !$task->is_completed, // toggle true/false
+        ]);
+
+        return redirect('/');
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect('/');
+    }
+}
